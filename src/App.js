83198ec458695f1f0445d2f3746b90f9bdae0762
Alfred Ödling <alfred.odling@hotmail.se>
2018-09-14 10:17:50 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/css/app.css'
+import React from 'react'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+         Hello!
+      </div>
+    )
+  }
+}
